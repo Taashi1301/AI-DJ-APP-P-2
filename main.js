169c17fadc03bcd933d6b173c6de0554f26ea30e
Taashi1301 <72211441+Taashi1301@users.noreply.github.com>
2020-12-15 18:30:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+sound="";
+function preload(){
+sound=loadSound("music.mp3");
+}
+
+function setup(){
+canvas=createCanvas(300, 300);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+image(video, 0, 0, 300, 300);
+}
+
